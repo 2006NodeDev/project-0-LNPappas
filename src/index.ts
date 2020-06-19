feab9e50272415c4df88686e5c0189a8261d1878
LNPappas <LPappas@gmail.com>
2020-06-19 13:57:12 -0700

    initialize project, installed libraries, updated package.json, added src & index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/* 
+    Start project by creating and installing necessary librarys. 
+    npm init
+    npm install express
+    npm install -D typescript ts-node @types/express
+    npm install express-session
+    npm install -D @types/express-session nodemon
+    in package.json scripts{ "start": "nodemon --exec ts-node src/index.ts", }
+*/ 
