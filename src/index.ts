2aeebdd8bdd98690408112bb5a93ccd152cab4b7
LNPappas <LPappas@gmail.com>
2020-06-22 10:29:58 -0700

    created: HttpError, AuthenticationFailureError, AuthorizationFailureError, authentication-middleware, authorization-middleware, session-middleware, updated index.ts with comments, updated package.json with crypto module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
+/*
+    # Expense Reimbursement System (ERS) API
+    The Expense Reimbursement System (ERS) will manage 
+    the process of reimbursing employees for expenses 
+    incurred while on company time. All employees in the 
+    company can login and submit requests for 
+    reimbursement and view their past tickets and pending 
+    requests. Finance managers can log in and view all 
+    reimbursement requests and past history for all 
+    employees in the company. Finance managers are 
+    authorized to approve and deny requests for expense 
+    reimbursement.
+*/
+
 /* 
-    Start project by creating and installing necessary librarys. 
-    npm init
-    npm install express
-    npm install -D typescript ts-node @types/express
-    npm install express-session
-    npm install -D @types/express-session nodemon
-    in package.json scripts{ "start": "nodemon --exec ts-node src/index.ts", }
+    Start project by creating and installing necessary libraries: 
+        npm init
+        npm install express
+        npm install -D typescript ts-node @types/express
+        npm install express-session
+        npm install -D @types/express-session nodemon
+        in package.json scripts{ "start": "nodemon --exec ts-node src/index.ts", }
 */ 
