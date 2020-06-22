08f6dce44921cb2769b9b2bf114c95f29e5822cb
LNPappas <LPappas@gmail.com>
2020-06-22 14:08:53 -0700

    did a ton of unecessary stuff than undid it and realized I just needed to add tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,61 @@
         npm init
         npm install express
         npm install -D typescript ts-node @types/express
+        copy in tsconfig.json
         npm install express-session
         npm install -D @types/express-session nodemon
+        in package.json: "main": "src/index.ts"
         in package.json scripts{ "start": "nodemon --exec ts-node src/index.ts", }
 */ 
+
+import express, { Request, Response } from 'express';
+import { sessionMiddleware } from './middleware/session-middleware';
+import { AuthenticationFailureError } from './errors/AuthenticationFailureError';
+import { userRouter, users } from './routers/user-router';
+
+// returns pre-build express app, must run first
+const app = express();
+
+// middleware: body-parser, only parses json
+app.use(express.json());
+
+// middleware: creates unique sessions 
+app.use(sessionMiddleware);
+
+app.use('/user', userRouter);
+
+// User login, validate authentication
+app.post('/login', (req:Request, res:Response)=>{
+
+    // get username and password from body of request
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // if username or password are empty throw error
+    if(!username || !password){
+        throw new AuthenticationFailureError();
+
+    // if username & password are valid & found in database, request a session for the user & respond with the user info in json format
+    } else{
+        let found = false
+        for(const user of users){
+            // proper credetials result in unique session
+            if(user.username === username && user.password === password){
+                req.session.user = user;
+                res.json(user);
+                found = true;
+            }
+        }
+        // if username & password not found it database throw an error
+        if(!found){
+            throw new AuthenticationFailureError();
+        }
+
+    }
+})
+
+/* start server on port 2006
+ access through browser or postman
+ http://localhost:2006 or http://127.0.0.1
+*/
+app.listen(2006, ()=> console.log('Server started...'));
---
+++ src/routers/user-router.ts
@@ -0,0 +1,119 @@
+
+import express, { Request, Response, NextFunction, response } from 'express';
+import { User } from '../models/User'
+import { authenticationMiddleware } from '../middleware/authentication-middleware';
+import { authorizationMiddleware } from '../middleware/authorization-middleware';
+
+export const userRouter = express.Router();
+
+userRouter.use(authenticationMiddleware); // authenticates user
+
+userRouter.get('/', authorizationMiddleware(['admin']), (req:Request, res:Response, next:NextFunction)=>{
+    res.json(users); // respond to get request with user array if authorization === admin
+})
+
+userRouter.get('/:id', authorizationMiddleware(['admin', 'finance-manager']), (req:Request, res:Response, next:NextFunction)=>{
+    let {id} = req.params;
+    if(isNaN(+id)){
+        res.status(400).send("Must be a number");
+    } else{
+        let found = false;
+        for(const user of users){
+            if(user.userId === +id){
+                res.json(user);
+                found = true;
+            }
+        }
+        if(!found){
+            res.status(404).send('User not found')
+        }
+    }
+})
+
+userRouter.patch('/', authorizationMiddleware(['admin']), (req:Request, res:Response, next:NextFunction)=>{
+    let id = req.body.userId;
+
+    if(!id){
+        throw response.status(404).send('User not found')
+    }else if(isNaN(+id)){
+        res.status(400).send("User Id must be a number");
+    }else{
+        let found = false;
+        for(const user of users){
+            if(user.userId === +id){
+
+                let username = req.body.username;
+                let password = req.body.password;
+                let firstName = req.body.firstName;
+                let lastName = req.body.lastName;
+                let email = req.body.email;
+                let role = req.body.role;
+
+                if(username){
+                    user.username = username;
+                }
+                if(password){
+                    user.password = password;
+                }
+                if(firstName){
+                    user.firstName = firstName;
+                }
+                if(lastName){
+                    user.lastName = lastName;
+                }
+                if(email){
+                    user.email = email;
+                }
+                if (role){
+                    user.role = role;
+                }
+
+                res.json(user);
+                found = true;
+            }
+        }
+        if(!found){
+            res.status(404).send('User not found')
+        }
+    }
+
+})
+
+export let users:User[] = [
+    {
+        userId:1,
+        username:'TheSpy',
+        password:'password',
+        firstName:'Harriet',
+        lastName:'Daniels',
+        email:'TheSpy@amin.com',
+        role:{
+            roleId: 1,
+            role: "admin"
+        },
+    },
+    {
+        userId:2,
+        username:'Wilber',
+        password:'password',
+        firstName:'Charlotte',
+        lastName:'Web',
+        email:'Wilber@financeManager.com',
+        role:{
+            roleId: 2,
+            role: "finanace-manager"
+        },
+    },
+    {
+        userId:1,
+        username:'Bucks',
+        password:'password',
+        firstName:'Barry',
+        lastName:'Carbuckle',
+        email:'BCarBucks@user.com',
+        role:{
+            roleId: 3,
+            role: "user"
+        },
+    },
+]
